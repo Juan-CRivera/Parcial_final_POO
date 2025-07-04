6fae8b03a83d9c8e04dd13d7f4d937074a114c32
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2025-07-04 13:00:06 -0600

    avance_cliente_cuadrado

--- ClienteCuadrado/src/main/java/clientecuadrado/ClienteCuadrado.java
+++ ClienteCuadrado/src/main/java/clientecuadrado/ClienteCuadrado.java
@@ -4,13 +4,42 @@
 
 package clientecuadrado;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  *
  * @author David Ventura
  */
+
 public class ClienteCuadrado {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       try {
+            Socket socket = new Socket("localhost", 5000);
+            System.out.println("Conectado al servidor en el puerto 5000.");
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter writer = new PrintWriter(outputStream, true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            
+            
+            
+            
+            
+
+            
+        } catch (IOException e) {
+            
+        }
     }
 }
