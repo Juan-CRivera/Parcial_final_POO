8bb8eaf5dd40e5bd7b6f0d2e0d39e81b452fd23a
Juan-CRivera <00029324@uca.edu.sv>
2025-07-04 12:49:35 -0600

    correciones

--- Parcial_Final_POO/src/main/java/com/mycompany/parcial_final_poo/ManejadorCliente.java
+++ Parcial_Final_POO/src/main/java/com/mycompany/parcial_final_poo/ManejadorCliente.java
@@ -4,10 +4,60 @@
  */
 package com.mycompany.parcial_final_poo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author juank
  */
-public class ManejadorCliente {
-    
+public class ManejadorCliente implements Runnable {
+    private Socket socketCliente;
+
+    public ManejadorCliente(Socket socketCliente) {
+        this.socketCliente = socketCliente;
+    }
+    @Override
+    public void run() {
+        try {
+            InputStream inputStream = socketCliente.getInputStream();
+            OutputStream outputStream = socketCliente.getOutputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            PrintWriter writer = new PrintWriter(outputStream, true);
+
+            String nombre = reader.readLine();
+            String numero = reader.readLine();
+            
+             System.out.println("Cliente " + nombre + " conectado.");
+            
+            System.out.println("Nombre recibido: " + nombre);
+            System.out.println("Numero recibido:" + numero);
+            
+            writer.println("Bienvenido, " + nombre + "!");
+            
+           try {
+                int numeroStr = Integer.parseInt(numero);
+                int cuadrado = numeroStr * numeroStr;
+                writer.println("El cuadrado de " + numero + " es: " + cuadrado);
+            } catch (NumberFormatException e) {
+                writer.println("Error: el número ingresado no es válido.");
+            }
+
+            
+            LocalDateTime ahora = LocalDateTime.now();
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            writer.println("Fecha y hora del servidor: " + ahora.format(formato)); 
+            
+            socketCliente.close();
+        } catch (IOException e) {
+            System.out.println("Error con cliente: " + e);
+        }
+    }
 }
--- Parcial_Final_POO/src/main/java/com/mycompany/parcial_final_poo/Parcial_Final_POO.java
+++ Parcial_Final_POO/src/main/java/com/mycompany/parcial_final_poo/Parcial_Final_POO.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.parcial_final_poo;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author LENOVO
@@ -11,6 +15,18 @@ package com.mycompany.parcial_final_poo;
 public class Parcial_Final_POO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Servidor esperando conexiones en puerto 5000...");
+
+            while (true) {
+                Socket socketCliente = serverSocket.accept();
+                System.out.println("Cliente conectado desde: " + socketCliente.getInetAddress());
+                new Thread(new ManejadorCliente(socketCliente)).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e);
+        }
     }
-}
+    }
+
