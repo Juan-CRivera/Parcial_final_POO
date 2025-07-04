096099f32ba8ba1d8069a0f379354bc626604e35
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2025-07-04 11:12:58 -0600

    creacion proyecto

---
+++ Parcial_Final_POO/src/main/java/com/mycompany/parcial_final_poo/Parcial_Final_POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial_final_poo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Parcial_Final_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
